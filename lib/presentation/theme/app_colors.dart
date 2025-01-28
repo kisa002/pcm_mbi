9b2d7db86398f3fee7f85c2ed53a8531b6d0ae56
Holy-Knight <vnycall74@naver.com>
2025-01-28 19:49:34 +0900

    feat: default app colors

---
+++ lib/presentation/theme/app_colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+class AppColors {
+  static const primary = Color(0xFF7272FF);
+
+  static const white = Color(0xFFFFFFFF);
+  static const black = Color(0xFF333333);
+  static const grey = Color(0xFFEFEFEF);
+}
