81322aeb6a4304f23ff3277dcedd4c181a6c1e2c
Holy-Knight <vnycall74@naver.com>
2025-01-28 21:00:44 +0900

    feat: age and weight info on BMI screen

--- lib/presentation/screens/bmi/bmi_controller.dart
+++ lib/presentation/screens/bmi/bmi_controller.dart
@@ -1,5 +1,20 @@
 import 'package:get/get.dart';
 
 class BmiController extends GetxController {
+  final age = 20.obs;
+  final weight = 50.obs;
 
+  void increaseAge() => age.value++;
+  void decreaseAge() {
+    if (age.value > 0) {
+      age.value--;
+    }
+  }
+
+  void increaseWeight() => weight.value++;
+  void decreaseWeight() {
+    if (weight.value > 0) {
+      weight.value--;
+    }
+  }
 }
--- lib/presentation/screens/bmi/bmi_screen.dart
+++ lib/presentation/screens/bmi/bmi_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:pcm_bmi/presentation/components/info_card.dart';
+import 'package:pcm_bmi/presentation/screens/bmi/bmi_controller.dart';
 import 'package:pcm_bmi/presentation/theme/app_colors.dart';
 
 class BmiScreen extends StatelessWidget {
@@ -7,43 +9,49 @@ class BmiScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      color: AppColors.primary,
-      child: SafeArea(
-        child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
-          child: Column(
-            children: [
-              Text(
-                'PCM BMI',
-                style: TextStyle(
-                  color: AppColors.white,
-                  fontSize: 24,
-                  fontWeight: FontWeight.w700,
+    final controller = Get.put(BmiController());
+
+    return Obx(
+      () => Material(
+        color: AppColors.primary,
+        child: SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+            child: Column(
+              children: [
+                Text(
+                  'PCM BMI',
+                  style: TextStyle(
+                    color: AppColors.white,
+                    fontSize: 24,
+                    fontWeight: FontWeight.w700,
+                  ),
                 ),
-              ),
-              SizedBox(height: 32),
-              Flex(
-                direction: Axis.horizontal,
-                children: [
-                  Flexible(
-                    child: InfoCard(
-                      name: 'Age',
-                      value: "18",
-                      onTap: (){},
+                SizedBox(height: 32),
+                Flex(
+                  direction: Axis.horizontal,
+                  children: [
+                    Flexible(
+                      child: InfoCard(
+                        name: 'Age',
+                        value: controller.age.string,
+                        onDecrease: controller.decreaseAge,
+                        onIncrease: controller.increaseAge,
+                      ),
                     ),
-                  ),
-                  SizedBox(width: 24),
-                  Flexible(
-                    child: InfoCard(
-                      name: 'Weight (KG)',
-                      value: "52",
-                      onTap: (){},
+                    SizedBox(width: 24),
+                    Flexible(
+                      child: InfoCard(
+                        name: 'Weight (KG)',
+                        value: controller.weight.string,
+                        onDecrease: controller.decreaseWeight,
+                        onIncrease: controller.increaseWeight,
+                      ),
                     ),
-                  ),
-                ],
-              ),
-            ],
+                  ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
