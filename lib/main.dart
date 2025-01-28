ab4585851b98016f8fc5d589256036f5d7b67450
Holy-Knight <vnycall74@naver.com>
2025-01-28 20:02:11 +0900

    fix: change MaterialApp to GetMaterialApp for using GetX

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:pcm_bmi/presentation/screens/onboarding/onboarding_screen.dart';
 import 'package:pcm_bmi/presentation/theme/app_colors.dart';
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       title: 'PCM BMI',
       theme: ThemeData(
         fontFamily: 'Pretendard',
