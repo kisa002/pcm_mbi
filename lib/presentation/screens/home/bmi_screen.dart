981977e08afb1e2d0c3626a474b194b134e9afb7
Holy-Knight <vnycall74@naver.com>
2025-01-28 20:48:30 +0900

    design: age and weight card on BMI screen

--- lib/presentation/screens/home/bmi_screen.dart
+++ lib/presentation/screens/home/bmi_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pcm_bmi/presentation/components/info_card.dart';
 import 'package:pcm_bmi/presentation/theme/app_colors.dart';
 
 class BmiScreen extends StatelessWidget {
@@ -9,8 +10,41 @@ class BmiScreen extends StatelessWidget {
     return Material(
       color: AppColors.primary,
       child: SafeArea(
-        child: Center(
-          child: Text('BMI Screen'),
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+          child: Column(
+            children: [
+              Text(
+                'PCM BMI',
+                style: TextStyle(
+                  color: AppColors.white,
+                  fontSize: 24,
+                  fontWeight: FontWeight.w700,
+                ),
+              ),
+              SizedBox(height: 32),
+              Flex(
+                direction: Axis.horizontal,
+                children: [
+                  Flexible(
+                    child: InfoCard(
+                      name: 'Age',
+                      value: "18",
+                      onTap: (){},
+                    ),
+                  ),
+                  SizedBox(width: 24),
+                  Flexible(
+                    child: InfoCard(
+                      name: 'Weight (KG)',
+                      value: "52",
+                      onTap: (){},
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
