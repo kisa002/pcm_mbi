cfd4a9844a2c43866473c4da4efe5ceac8183d96
Holy-Knight <vnycall74@naver.com>
2025-01-28 20:00:03 +0900

    feat: onboarding screen

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: AppColors.primary),
         useMaterial3: true,
       ),
-      home: Placeholder(),
+      home: OnboardingScreen(),
       debugShowCheckedModeBanner: false,
     );
   }
